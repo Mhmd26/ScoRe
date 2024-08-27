627594e1c6b7940865dec392d6429ac8c9e2bae3
Mhmd26 <hamodey45@outlook.com>
2024-08-27 11:34:06 +0300

    Update gg.py

--- gg.py
+++ gg.py
@@ -1,25 +1,15 @@
-
 import subprocess
 
 def start_gunicorn():
-    # تحديد مسار مشروع Django، واسم المشروع (على سبيل المثال myproject)
-    project_path = '/path/to/your/django/project'
-    wsgi_module = 'myproject.wsgi:application'
-    
-    # تحديد عدد العمال والمنفذ الذي سيستمع عليه Gunicorn
     command = [
         'gunicorn',
         '--workers', '3',
         '--bind', '0.0.0.0:8000',
-        wsgi_module
+        'myproject.wsgi:application'
     ]
-
-    # تشغيل Gunicorn
-    process = subprocess.Popen(command, cwd=project_path)
-    
+    process = subprocess.Popen(command)
     return process
 
 if __name__ == "__main__":
-    # بدء Gunicorn
     process = start_gunicorn()
     print(f'Gunicorn started with PID: {process.pid}')
