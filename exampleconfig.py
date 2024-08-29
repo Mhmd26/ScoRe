bf1b7f9318e26e0ebdcb952aea23be4296c23b93
Mhmd26 <hamodey45@outlook.com>
2024-08-29 17:47:56 +0300

    Update exampleconfig.py

--- exampleconfig.py
+++ exampleconfig.py
@@ -1,21 +1,16 @@
 from sample_config import Config
-
-
 class Development(Config):
     # get this values from the my.telegram.org
-    APP_ID = 6
-    API_HASH = "eb06d4abfb49dc3eeb1aeb98ae0f581e"
+    APP_ID = 23970174
+    API_HASH = "f1db2e38b2c73448ef09c504187e888d"
     # the name to display in your alive message
-    ALIVE_NAME = "Your value"
+    ALIVE_NAME = "Mohamed"
     # create any PostgreSQL database (i recommend to use elephantsql) and paste that link here
-    DB_URI = "Your value"
+    DB_URI = "postgresql://data_o0i3_user:wKaJ9qBGpt9bRsgHGLqamHftqKbzaCwM@dpg-cr7odrtumphs73af2ug0-a.singapore-postgres.render.com/data_o0i3"
     # After cloning the repo and installing requirements do python3 telesetup.py an fill that value with this
-    STRING_SESSION = "Your value"
+    STRING_SESSION = "1ApWapzMBuzmugqYqk_g0KPuNal6yZMWrbvmwhT6-FShhMP5hftXqXmaC9Xr3mTNPnl80a8_tfZqCwheymGNJuub59gUaTuH047NQC_TLUfgcHG_wI56B11Lx89OLKWOhUz3V6RnVHwiruYWFniMkKjhtaAuEknwutEAJJeVoPvyPfBOjyVIyDGIqt5x4mZP6Y5la-vbrUh0VCLuGDnA2ts1HB2-3ZWXMcHzhOyQMBLGd6JxZWEj99DZqM2904nBSwyx5kx7gOCR3I1oOuG_vQ232OUAdbzdOoh5EeFu8gHpZXu23myD5xwCclEf8RwdBIrSAvhwrPg6jWM96F8JLZOOVeubAAiM="
     # create a new bot in @botfather and fill the following vales with bottoken and username respectively
-    TG_BOT_TOKEN = "Your value"
-    TG_BOT_USERNAME = "Your value"
-    # create a private group and a rose bot to it and type /id and paste that id here (replace that -100 with that group id)
-    PRIVATE_GROUP_BOT_API_ID = -100
+    TG_BOT_TOKEN = "6704219679:AAGuFeer0hkaeAvCu3_27wOI-h7p4FR7eJI"
     # command handler
     COMMAND_HAND_LER = "."
     # sudo enter the id of sudo users userid's in that array
