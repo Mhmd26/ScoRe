d844a3ba2c52a24060881aa28b788afc61d796f8
Mhmd26 <hamodey45@outlook.com>
2024-09-15 23:35:23 +0300

    Update يوتيوب.py

--- JoKeRUB/plugins/يوتيوب.py
+++ JoKeRUB/plugins/يوتيوب.py
@@ -391,3 +391,32 @@ async def _(event):
             conv.chat_id, [msg_start.id, r.id, msg.id, details.id, video.id]
         )
         await event.delete()
+
+@l313l.on(admin_cmd(pattern="يوتيوب(?: |$)(.*)"))
+async def _(event):
+    if event.fwd_from:
+        return
+    r_link = event.pattern_match.group(1)
+    if "youtube.com" not in r_link and "youtu.be" not in r_link:
+        await event.edit("**✎┊‌يجب وضع رابط يوتيوب فقط مع الأمر**")
+    else:
+        await event.edit("**✎┊‌تتم المعالجة انتظر قليلا**")
+        chat = "@YouTubeBot"  # تغيير اسم البوت إلى البوت المناسب لـ YouTube
+        async with bot.conversation(chat) as conv:
+            try:
+                msg_start = await conv.send_message("/start")
+                r = await conv.get_response()
+                msg = await conv.send_message(r_link)
+                details = await conv.get_response()
+                video = await conv.get_response()
+                """ قناة الجوكر  **العقرب |  𝗦𝗰𝗼𝗿𝗽𝗶𝗼 🦂**"""
+                await bot.send_read_acknowledge(conv.chat_id)
+            except YouBlockedUserError:
+                await event.edit("✎┊‌الغـي حـظر هـذا البـوت و حـاول مجـددا @YouTubeBot")
+                return
+            await bot.send_file(event.chat_id, video)
+            await event.client.delete_messages(
+                conv.chat_id, [msg_start.id, r.id, msg.id, details.id, video.id]
+            )
+            await event.delete()
+            
