807beeaedfd1633831a6a5661d78530fe6b4e349
Mhmd26 <hamodey45@outlook.com>
2024-09-16 19:46:06 +0300

    Update الترجمة.py

--- JoKeRUB/plugins/الترجمة.py
+++ JoKeRUB/plugins/الترجمة.py
@@ -1,138 +1,163 @@
-from asyncio import sleep
-import requests
-import json
-import os
-from JoKeRUB.helpers.functions.functions import translate
+# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~# CatUserBot #~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~#
+# Copyright (C) 2020-2023 by TgCatUB@Github.
+
+# This file is part of: https://github.com/TgCatUB/catuserbot
+# and is released under the "GNU v3.0 License Agreement".
+
+# Please see: https://github.com/TgCatUB/catuserbot/blob/master/LICENSE
+# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~#
+
+from googletrans import LANGUAGES, Translator
+
+
 from JoKeRUB import l313l
-from telethon import events, types
-from ..sql_helper.globals import addgvar, delgvar, gvarstatus
+
 from ..core.managers import edit_delete, edit_or_reply
-from ..helpers.functions import soft_deEmojify
+from ..helpers.functions.functions import getTranslate
+from ..sql_helper.globals import addgvar, gvarstatus
+from . import BOTLOG, BOTLOG_CHATID, soft_deEmojify
 
-langs = {
-    'عربي': 'ar',
-    'فارسي': 'fa',
-    'بلغاري': 'bg',
-    'صيني مبسط': 'zh',
-    'صيني تقليدي': 'zh-TW',
-    'كرواتي': 'hr',
-    'دنماركي': 'da',
-    'الماني': 'de',
-    'انجليزي': 'en',
-    'فنلندي': 'fil',
-    'فرنسي': 'fr',
-    'يوناني': 'el',
-    'هنغاري': 'hu',
-    'كوري': 'ko',
-    'ايطالي': 'it',
-    'ياباني': 'ja',
-    'نرويجي': 'no',
-    'بولندي': 'pl',
-    'برتغالي': 'pt',
-    'روسي': 'ru',
-    'سلوفيني': 'sl',
-    'اسباني': 'es',
-    'سويدي': 'sv',
-    'تركي': 'tr',
-    'هندي': 'ur',
-    'كردي': 'ku',
-}
+plugin_category = "utils"
 
-async def gtrans(text, lan):
-    try:
-        response = translate(text, lang_tgt=lan)
-        if not response or not isinstance(response, (str, bytes)):
-            return "تلقى استجابة غير صالحة من خدمة الترجمة"
-        return response
-    except Exception as er:
-        return f"حدث خطأ \n{er}"
-
-@l313l.ar_cmd(pattern="event")
-async def handle_event(event):
-    if event.reply_to_msg_id:
-        m = await event.get_reply_message()
-        with open("reply.txt", "w") as file:
-            file.write(str(m))
-        await event.client.send_file(event.chat_id, "reply.txt")
-        os.remove("reply.txt")
 
 @l313l.ar_cmd(
     pattern="ترجمة ([\s\S]*)",
-    command=("ترجمة", "tools"),
+    command=("ترجمة", plugin_category),
     info={
         "header": "To translate the text to required language.",
-        "note": "For language codes check [this link](https://bit.ly/2SRQ6WU)",
+        "note": "For langugage codes check [this link](https://bit.ly/2SRQ6WU)",
         "usage": [
             "{tr}tl <language code> ; <text>",
             "{tr}tl <language codes>",
         ],
-        "examples": "{tr}tl en ; Catuserbot is one of the popular bot",
+        "examples": "{tr}tl te ; Catuserbot is one of the popular bot",
     },
 )
-async def translate_text(event):
+async def _(event):
     "To translate the text."
-    input_str = event.pattern_match.group(1).strip()
-    
+    input_str = event.pattern_match.group(1)
     if event.reply_to_msg_id:
         previous_message = await event.get_reply_message()
         text = previous_message.message
         lan = input_str or "en"
     elif ";" in input_str:
-        lan, text = input_str.split(";", 1)
-        lan = lan.strip()
-        text = text.strip() if text else ""  # Ensure text is not None
+        lan, text = input_str.split(";")
     else:
         return await edit_delete(
-            event, "** قم بالرد على الرسالة للترجمة **", time=5
+            event, "`.tl LanguageCode` as reply to a message", time=5
         )
-    
-    text = soft_deEmojify(text) or ""  # Ensure text is not None
-
+    text = soft_deEmojify(text.strip())
+    lan = lan.strip()
+    Translator()
     try:
-        trans = await gtrans(text, lan)
-        if not trans:
-            return await edit_delete(event, "**تحقق من رمز اللغة !, لا يوجد هكذا لغة**")      
-        if isinstance(trans, str):
-            output_str = f"**تمت الترجمة من ar الى {lan}**\n`{trans}`"
-            await edit_or_reply(event, output_str)
-        else:
-            await edit_delete(event, f"**خطا:**\n`استجابة غير صالحة`", time=5)
+        translated = await getTranslate(text, dest=lan)
+        after_tr_text = translated.text
+        output_str = f"**TRANSLATED from {LANGUAGES[translated.src].title()} to {LANGUAGES[lan].title()}**\
+                \n`{after_tr_text}`"
+        await edit_or_reply(event, output_str)
     except Exception as exc:
-        await edit_delete(event, f"**خطا:**\n`{exc}`", time=5)
+        await edit_delete(event, f"**Error:**\n`{exc}`", time=5)
 
-@l313l.ar_cmd(pattern="(الترجمة الفورية|الترجمه الفوريه|ايقاف الترجمة|ايقاف الترجمه)")
-async def toggle_translation(event):
-    if gvarstatus("transnow"):
-        delgvar("transnow")
-        await edit_delete(event, "**✎┊‌ تم تعطيل الترجمه الفورية **")
+
+@l313l.ar_cmd(
+    pattern="trt(?: |$)([\s\S]*)",
+    command=("trt", plugin_category),
+    info={
+        "header": "To translate the text to required language.",
+        "note": "for this command set lanuage by `{tr}lang trt` command.",
+        "usage": [
+            "{tr}trt",
+            "{tr}trt <text>",
+        ],
+    },
+)
+async def translateme(trans):
+    "To translate the text to required language."
+    textx = await trans.get_reply_message()
+    message = trans.pattern_match.group(1)
+    if message:
+        pass
+    elif textx:
+        message = textx.text
     else:
-        addgvar("transnow", "Reda") 
-        await edit_delete(event, "**✎┊‌ تم تفعيل الترجمه الفورية**")
+        return await edit_or_reply(
+            trans, "`Give a text or reply to a message to translate!`"
+        )
+    TRT_LANG = gvarstatus("TRT_LANG") or "en"
+    try:
+        reply_text = await getTranslate(soft_deEmojify(message), dest=TRT_LANG)
+    except ValueError:
+        return await edit_delete(trans, "`Invalid destination language.`", time=5)
+    source_lan = LANGUAGES[f"{reply_text.src.lower()}"]
+    transl_lan = LANGUAGES[f"{reply_text.dest.lower()}"]
+    reply_text = f"**From {source_lan.title()}({reply_text.src.lower()}) to {transl_lan.title()}({reply_text.dest.lower()}) :**\n`{reply_text.text}`"
 
-@l313l.ar_cmd(pattern="لغة الترجمة")
-async def set_translation_language(event):
-    t = event.text.replace(".لغة الترجمة", "").strip()
-    try:  
-        lang = langs[t]
-    except KeyError:
-        return await edit_delete(event, "**✎┊‌ !تأكد من قائمة اللغات. لا يوجد هكذا لغة**")
-    addgvar("translang", lang)
-    await edit_delete(event, f"**✎┊‌ تم تغير لغة الترجمة الى {lang} بنجاح ✓ **")
+    await edit_or_reply(trans, reply_text)
+    if BOTLOG:
+        await trans.client.send_message(
+            BOTLOG_CHATID,
+            f"`Translated some {source_lan.title()} stuff to {transl_lan.title()} just now.`",
+        )
 
-@l313l.on(events.NewMessage(outgoing=True))
-async def auto_translate(event):
-    if gvarstatus("transnow"):
-        if event.media or isinstance(event.media, (types.MessageMediaDocument, types.MessageMediaInvoice)):
-            print("JoKeRUB")
-        else:
-            original_message = event.message.message
-            if original_message:
-                lang = gvarstatus("translang") or "en"
-                if lang:
-                    translated_message = await gtrans(soft_deEmojify(original_message.strip()), lang)
-                    if translated_message:
-                        await event.message.edit(translated_message)
-                    else:
-                        await event.message.edit("حدث خطأ أثناء الترجمة.")
-                else:
-                    await event.message.edit("لم يتم تحديد لغة الترجمة.")
+
+@l313l.ar_cmd(
+    pattern="lang (ai|trt|tocr) ([\s\S]*)",
+    command=("lang", plugin_category),
+    info={
+        "header": "To set language for trt/ai command.",
+        "description": "Check here [Language codes](https://bit.ly/2SRQ6WU)",
+        "options": {
+            "trt": "default language for trt command",
+            "tocr": "default language for tocr command",
+            "ai": "default language for chatbot(ai)",
+        },
+        "usage": "{tr}lang option <language codes>",
+        "examples": [
+            "{tr}lang trt te",
+            "{tr}lang tocr bn",
+            "{tr}lang ai hi",
+        ],
+    },
+)
+async def lang(value):
+    "To set language for trt comamnd."
+    arg = value.pattern_match.group(2).lower()
+    input_str = value.pattern_match.group(1)
+    if arg not in LANGUAGES:
+        return await edit_or_reply(
+            value,
+            f"`Invalid Language code !!`\n`Available language codes for TRT`:\n\n`{LANGUAGES}`",
+        )
+    LANG = LANGUAGES[arg]
+    if input_str == "trt":
+        addgvar("TRT_LANG", arg)
+        await edit_or_reply(
+            value, f"**Language for Translator changed to:** `{LANG.title()}`"
+        )
+    elif input_str == "tocr":
+        addgvar("TOCR_LANG", arg)
+        await edit_or_reply(
+            value, f"**Language for Translated Ocr changed to:** `{LANG.title()}`"
+        )
+    else:
+        addgvar("AI_LANG", arg)
+        await edit_or_reply(
+            value, f"**Language for Chatbot is changed to:** `{LANG.title()}`"
+        )
+    LANG = LANGUAGES[arg]
+
+    if BOTLOG and input_str == "trt":
+        await value.client.send_message(
+            BOTLOG_CHATID, f"**Language for Translator changed to:** `{LANG.title()}`"
+        )
+    if BOTLOG:
+        if input_str == "tocr":
+            await value.client.send_message(
+                BOTLOG_CHATID,
+                f"**Language for Translated Ocr changed to:** `{LANG.title()}`",
+            )
+        if input_str == "ai":
+            await value.client.send_message(
+                BOTLOG_CHATID,
+                f"**Language for Chatbot is changed to:** `{LANG.title()}`",
+            )
