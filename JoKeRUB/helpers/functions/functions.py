e2aa39f27c22eb7415a29b3102355e232aa9d4ec
Mhmd26 <hamodey45@outlook.com>
2024-09-16 20:16:51 +0300

    Add files via upload

--- JoKeRUB/helpers/functions/functions.py
+++ JoKeRUB/helpers/functions/functions.py
@@ -25,6 +25,7 @@ from telethon.tl.functions.contacts import UnblockRequest as unblock
 from ...Config import Config
 from ...sql_helper.globals import gvarstatus
 from ..resources.states import states
+
 imdb = IMDb()
 
 mov_titles = [
@@ -222,9 +223,30 @@ async def getTranslate(text, **kwargs):
             result = translator.translate(text, **kwargs)
         except Exception:
             translator = Translator()
-            await asyncio.sleep(0.1)
+            await sleep(0.1)
     return result
 
+def translate(*args, **kwargs):
+    headers = {
+        "Referer": "https://translate.google.co.in",
+        "User-Agent": "Mozilla/5.0 (Windows NT 10.0; WOW64) "
+        "AppleWebKit/537.36 (KHTML, like Gecko) "
+        "Chrome/47.0.2526.106 Safari/537.36",
+        "Content-Type": "application/x-www-form-urlencoded;charset=utf-8",
+    }
+    x = requests.post(
+        "https://translate.google.co.in/_/TranslateWebserverUi/data/batchexecute",
+        headers=headers,
+        data=_package_rpc(*args, **kwargs),
+    ).text
+    response = ""
+    data = json.loads(json.loads(x[4:])[0][2])[1][0][0]
+    subind = data[-2]
+    if not subind:
+        subind = data[-1]
+    for i in subind:
+        response += i[0]
+    return response
 
 def _get_value(stri):
     try:
