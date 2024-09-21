1fd8f5fcce1a45ad494323a3bf7773499096e269
Mhmd26 <hamodey45@outlook.com>
2024-09-21 21:41:19 +0300

    Update يوتيوب.py

--- JoKeRUB/plugins/يوتيوب.py
+++ JoKeRUB/plugins/يوتيوب.py
@@ -433,9 +433,8 @@ async def _(event):
             await conv.send_message(r_link)
             
             # الانتظار حتى يرد البوت على السؤال
-            response = await conv.get_response(timeout=20)  # يمكن تعديل الوقت حسب الحاجة
-            response = await conv.get_response(timeout=20)  # يمكن تعديل الوقت حسب الحاجة
-
+            response = await conv.get_response(timeout=30)  # يمكن تعديل الوقت حسب الحاجة
+    
             await bot.send_read_acknowledge(conv.chat_id)
         except YouBlockedUserError:
             await event.edit("✎┊‌الغـي حـظر هـذا البـوت و حـاول مجـددا @ScorGPTbot")
