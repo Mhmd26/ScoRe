78d19b776dcec6214f5e828409ba13942f2347b8
Mhmd26 <hamodey45@outlook.com>
2024-09-28 21:24:19 +0300

    Update chatbot.py

--- JoKeRUB/helpers/chatbot.py
+++ JoKeRUB/helpers/chatbot.py
@@ -1,5 +1,18 @@
 import asyncio
 import aiohttp
+import importlib.util
+import subprocess
+import sys
+
+def install(package):
+    subprocess.check_call([sys.executable, "-m", "pip", "install", package])
+
+def check_and_install(package):
+    if importlib.util.find_spec(package) is None:
+        print(f"Installing {package}...")
+        install(package)
+
+check_and_install("aiohttp")
 
 async def create_client():
     async with aiohttp.ClientSession() as session:
